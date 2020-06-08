7e798671e935644714332e16ec63550995c16140
nidnogg <henriquevt98@gmail.com>
2020-06-08 06:11:08 -0300

    next.js setup complete, babel issue fixed

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect, useRef } from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h3>
+        all I ever wanted
+      </h3>
+    </div>
+  );
+}
+
+export default Home;
