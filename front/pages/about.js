12bdaa910b03d03b12983be06f85f281cc4c9581
nidnogg <henriquevt98@gmail.com>
2020-06-18 07:34:18 -0300

    Added about page

--- front/pages/about.js
+++ front/pages/about.js
@@ -4,9 +4,25 @@ const About = () => {
   return (
     <Layout>
       <div>
-        <h3>
-        about page
-        </h3>
+        <div className="about-highlight">
+          <h2>
+            Seja bem vindo ao Papura Cadernos.
+          </h2>
+        </div>
+        <section className="about">
+          <h2> Nossa história </h2>
+          <br/>
+          <p>
+            Somos uma marca comprometida com a qualidade artesanal de nossos produtos. 
+            O papura tem suas origens como um caderninho minimalista
+            disponível para venda nas universidades do Rio de Janeiro.
+          </p>
+          <br/>
+          <h2> Valores</h2>
+          <br/>
+          <h2> Equipe </h2>
+          <br/>
+        </section>
       </div>
     </Layout>
   );
