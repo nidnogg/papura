bd9251c31d24cb26fc97162f12d60f091f807deb
nidnogg <henriquevt98@gmail.com>
2020-06-18 09:13:51 -0300

    More work on about page

--- front/pages/about.js
+++ front/pages/about.js
@@ -9,20 +9,42 @@ const About = () => {
             Seja bem vindo ao Papura Cadernos.
           </h2>
         </div>
-        <section className="about">
-          <h2> Nossa história </h2>
-          <br/>
-          <p>
-            Somos uma marca comprometida com a qualidade artesanal de nossos produtos. 
-            O papura tem suas origens como um caderninho minimalista
-            disponível para venda nas universidades do Rio de Janeiro.
-          </p>
-          <br/>
-          <h2> Valores</h2>
-          <br/>
-          <h2> Equipe </h2>
-          <br/>
-        </section>
+
+        <div className="about-grid">
+          <section className="about-col-0">
+            <h2> Nossa história </h2>
+            <br/>
+            <p>
+              Somos uma marca comprometida com a qualidade artesanal de nossos produtos. 
+              O papura tem suas origens como um caderninho minimalista
+              disponível para venda nas universidades do Rio de Janeiro.
+            </p>
+            <br/>
+            <h2> Valores</h2>
+            <br/>
+              <p>
+                Estamos dispostos a melhorar e lhe trazer o caderno mais simpático, agradável, e prático que você possa pedir.
+                Nossa busca pelo produto ideal está na raiz do nosso negócio. 
+              </p>  
+            <br/>
+          </section>
+
+          <section className="about-col-1">
+            <h2> Equipe </h2>
+              <br/>
+              <p>
+                  Estamos dispostos a melhorar e lhe trazer o caderno mais simpático, agradável, e prático que você possa pedir.
+                  Nossa busca pelo produto ideal está na raiz do nosso negócio. 
+                </p>  
+              <br/>
+              <h2> Fale conosco: </h2>
+              <br/>
+              <p>
+                  Seu feedback é a chave para um Papura melhor. Tem alguma dúvida ou sugestão? Fale conosco pelo form abaixo.
+                </p>  
+              <br/>
+          </section>
+        </div>
       </div>
     </Layout>
   );
