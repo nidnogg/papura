5baffa3af32a4080db9acab05e77f92e7345459c
nidnogg <henriquevt98@gmail.com>
2020-07-09 20:54:37 -0300

    about page 90% done

--- front/pages/about.js
+++ front/pages/about.js
@@ -3,49 +3,33 @@ import Layout from './components/layout';
 const About = () => {
   return (
     <Layout>
-      <div>
-        <div className="about-highlight">
-          <h1>
-            Seja bem vindo ao Papura Cadernos.
-          </h1>
-        </div>
+      <section className="about-row">
 
-        <div className="about-grid">
-          <section className="about-col-0">
-            <h2> Nossa história </h2>
-            <br/>
-            <p>
-              Somos uma marca comprometida com a qualidade artesanal de nossos produtos. 
-              O papura tem suas origens como um caderninho minimalista
-              disponível para venda nas universidades do Rio de Janeiro.
-            </p>
-            <br/>
-            <h2> Valores</h2>
-            <br/>
-              <p>
-                Estamos dispostos a melhorar e lhe trazer o caderno mais simpático, agradável, e prático que você possa pedir.
-                Nossa busca pelo produto ideal está na raiz do nosso negócio. 
-              </p>  
-            <br/>
-          </section>
+        <div className="about-image">
+          <img src="" alt="about-image"/>
 
-          <section className="about-col-1">
-            <h2> Equipe </h2>
-              <br/>
-              <p>
-                  Estamos dispostos a melhorar e lhe trazer o caderno mais simpático, agradável, e prático que você possa pedir.
-                  Nossa busca pelo produto ideal está na raiz do nosso negócio. 
-                </p>  
-              <br/>
-              <h2> Fale conosco: </h2>
-              <br/>
-              <p>
-                  Seu feedback é a chave para um Papura melhor. Tem alguma dúvida ou sugestão? Fale conosco pelo form abaixo.
-                </p>  
-              <br/>
-          </section>
         </div>
-      </div>
+        <div className="about-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Beatae esse error, minus possimus ullam placeat fugiat reprehenderit 
+            sint repellendus. Iure numquam blanditiis suscipit minus pariatur, fugiat 
+            vero dignissimos ipsam nemo?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Beatae esse error, minus possimus ullam placeat fugiat reprehenderit 
+            sint repellendus. Iure numquam blanditiis suscipit minus pariatur, fugiat 
+            vero dignissimos ipsam nemo? <br />
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Beatae esse error, minus possimus ullam placeat fugiat reprehenderit 
+            sint repellendus. Iure numquam blanditiis suscipit minus pariatur, fugiat 
+            vero dignissimos ipsam nemo?
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Beatae esse error, minus possimus ullam placeat fugiat reprehenderit 
+            sint repellendus. Iure numquam blanditiis suscipit minus pariatur, fugiat 
+            vero dignissimos ipsam nemo?
+          </p>
+        </div>
+      </section>
     </Layout>
   );
 }
