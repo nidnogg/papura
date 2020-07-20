835e19dceb4b8ba5872fc9ed3a567729e2756691
nidnogg <henriquevt98@gmail.com>
2020-07-20 16:36:32 -0300

    carousel

--- front/pages/products/product-0.js
+++ front/pages/products/product-0.js
@@ -1,13 +1,11 @@
 import Layout from '../components/layout';
+import ProductPage from '../components/productPage';
+
 
 const Product = () => {
   return (
     <Layout>
-      <div>
-        <h3>
-          product page
-        </h3>
-      </div>
+      <ProductPage />
     </Layout>
   );
 }
