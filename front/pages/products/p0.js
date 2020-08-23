8c43da63c32a0bd82a81ea456e984250160409c2
nidnogg <henriquevt98@gmail.com>
2020-08-23 10:48:16 -0300

    added compacto images

--- front/pages/products/p0.js
+++ front/pages/products/p0.js
@@ -1,11 +1,52 @@
 import Layout from '../components/layout';
-import ProductGrid from '../components/productGrid';
+import Link from "next/link";
 
+const Square = props => {
+  const {id} = props;
+  const line = 0;
+  return (
+    <Link href={`/products/p${line}${id}`}>
+
+      <img className="square" src={`/comp/${id}.jpg`} alt="notebook"></img> 
+      {/*
+      <div className="square">
+        <img src={`/comp/${id}`} alt="notebook"></img>
+      </div>
+       */}
+    </Link>
+  );
+}
 
 const Product = () => {
   return (
     <Layout>
-      <ProductGrid line={0}/>
+      <div className='square-grid'>
+        <div className="square-row">
+          <Square id={0}></Square>
+          <Square id={1}></Square>
+          <Square id={2}></Square>
+          <Square id={3}></Square>
+        </div>
+
+        <div className="square-row">
+          <Square id={4}></Square>
+          <Square id={5}></Square>
+          <Square id={6}></Square>
+          <Square id={7}></Square>
+        </div>
+
+        <div className="square-row">
+          <Square id={8}></Square>
+          <Square id={9}></Square>
+          <Square id={10}></Square>
+          <Square id={11}></Square>
+        </div>
+
+        <div className="square-row">
+          <Square id={12}></Square>
+          <Square id={13}></Square>
+        </div>
+      </div>
     </Layout>
   );
 }
